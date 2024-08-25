5ac676dd5509f89ac7ccae52c14d9ffd224feba6
golda <z0504199417@gmail.com>
2024-08-25 18:30:53 +0300

    change a few things in the routing at the server

--- server/server.js
+++ server/server.js
@@ -1,88 +1,36 @@
-// require("dotenv").config();
-// const cookieParser = require("cookie-parser");
-// const express = require("express");
-// const mongoose = require("mongoose");
-// const app = express();
-// const cors = require("cors");
-// const corsOptions = require("./config/corsOption");
-// const connectDB = require("./config/dbConn");
-// const PORT = 1000;
-// //const PORT=process.env.PORT||33
-// connectDB();
-// app.use(cors(corsOptions));
-// app.use(cookieParser());
-
-// app.use(express.json());
-// app.use(express.static("public"));
-// app.get("/", (req, res) => {
-//   res.send("home 1 page");
-// });
-
-
-// app.use("/api/listwords", require("./routes/listWordsRoute"));
-// app.use("/api/auth", require("./routes/authRoutes"));
-// app.use("/api/user", require("./routes/userRoute"));
-// app.use("/api/companies", require("./routes/compamyRoute"));
-// app.use("/api/schools", require("./routes/schoolRoute"));
-// app.use("/api/users", require("./routes/userRoute"));
-// app.use("/api/test",require("./routes/testRoute"))
-// mongoose.connection.once("open", () => {
-//   console.log("connect to db success");
-//   app.listen(PORT, () => {
-//     console.log(`server running on port ${PORT}`);
-
-//   });
-// });
-
-// mongoose.connection.on("error", (err) => {
-//   console.log("****************DB ERROR*****************");
-//   console.log(err);
-// });
-
-
 require("dotenv").config();
 const cookieParser = require("cookie-parser");
 const express = require("express");
 const mongoose = require("mongoose");
-const path = require("path");  // הוספת מודול path
 const app = express();
 const cors = require("cors");
 const corsOptions = require("./config/corsOption");
 const connectDB = require("./config/dbConn");
-const PORT = process.env.PORT || 1000;
-
+const PORT = 1000;
+//const PORT=process.env.PORT||33
 connectDB();
 app.use(cors(corsOptions));
 app.use(cookieParser());
 
 app.use(express.json());
-
-// הגדרת נתיב לתיקייה הציבורית שמכילה את ה-build של אפליקציית ה-React
-app.use(express.static(path.join(__dirname, "public")));
-
+app.use(express.static("public"));
 app.get("/", (req, res) => {
   res.send("home 1 page");
 });
 
-// הוספת כל הנתיבים של ה-API
+
 app.use("/api/listwords", require("./routes/listWordsRoute"));
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/user", require("./routes/userRoute"));
 app.use("/api/companies", require("./routes/compamyRoute"));
 app.use("/api/schools", require("./routes/schoolRoute"));
 app.use("/api/users", require("./routes/userRoute"));
-app.use("/api/test", require("./routes/testRoute"));
-
-// ניתוב מחדש לכל שאר הבקשות
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-});
-
-// חיבור למסד הנתונים והאזנה לשרת
+app.use("/api/test",require("./routes/testRoute"))
 mongoose.connection.once("open", () => {
   console.log("connect to db success");
   app.listen(PORT, () => {
     console.log(`server running on port ${PORT}`);
+
   });
 });
 
